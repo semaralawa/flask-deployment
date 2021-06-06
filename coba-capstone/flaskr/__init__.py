fc07b8c8db127860471b45438510fe305489a47b
semaralawa <dewasanta2@gmail.com>
2021-06-06 06:34:31 +0000

    edit init.py

--- coba-capstone/flaskr/__init__.py
+++ coba-capstone/flaskr/__init__.py
@@ -39,3 +39,7 @@ def create_app(test_config=None):
         return 'Hello, World!'
 
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(port=5000, debug=True, host='0.0.0.0', usereloader=True)
--- coba-capstone/main.py
+++ coba-capstone/main.py
@@ -1,5 +1,5 @@
 import os
-import flask
+#import flask
 from flask import Flask
 
 
